68cbc17e76feec0d74163bb5812997fcab60b97e
unknown <biradarkailas@gmail.com>
2021-04-27 00:04:35 +0530

    changes commited

--- src/main/java/com/payfix/packages/domain/User.java
+++ src/main/java/com/payfix/packages/domain/User.java
@@ -82,7 +82,7 @@ public class User extends AbstractAuditingEntity implements Serializable {
     @JsonIgnore
     @ManyToMany
     @JoinTable(
-        name = "jhi_user_authority",
+        name = "user_authority",
         joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
         inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")}
     )
